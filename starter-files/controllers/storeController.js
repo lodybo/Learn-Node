f0f052816116dd95c8aef034edef4ad9ad4e93d5
Lody Borgers <hi@lodybo.nl>
2017-05-30 17:16:14 +0200

    Adjusted indentation, because it bothered me..

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,9 @@
 exports.homePage = (req, res) => {
-    res.render('index');
+  res.render('index');
+};
+
+exports.addStore = (reg, res) => {
+  res.render('editStore', {
+    title: 'Add Store'
+  });
 };
--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -4,27 +4,27 @@ const slug = require("slugs");
 mongoose.Promise = global.Promise;
 
 const storeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: 'Please enter a store name!'
-    },
-    slug: String,
-    descirption: {
-        type: String,
-        trim: true
-    },
-    tags: [String]
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  descirption: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
 });
 
 storeSchema.pre('save', function (next) {
-    if (!this.isModified('name')) {
-        next();
-        return;
-    }
-    
-    this.slug = slug(this.name);
+  if (!this.isModified('name')) {
     next();
+    return;
+  }
+
+  this.slug = slug(this.name);
+  next();
 });
 
 module.exports = mongoose.model('Store', storeSchema);
